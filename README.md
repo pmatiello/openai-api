2760e93c8618d64efa618e9e377a677a59a6fee0
Pedro Matiello <pedro@pmatiello.me>
2023-04-20 22:02:09 -0300

    Add build script.

--- README.md
+++ README.md
@@ -39,6 +39,20 @@ The following command will execute the unit tests:
 % clj -X:test
 ```
 
+### Building
+
+The following command will build a jar file:
+
+```
+% clj -T:build jar
+```
+
+To clean a previous build, run:
+
+```
+% clj -T:build clean
+```
+
 ## Contribution Policy
 
 This software is open-source, but closed to contributions.
---
+++ build.clj
@@ -0,0 +1,22 @@
+(ns build
+  (:require [clojure.tools.build.api :as b]))
+
+(def lib 'pmatiello/openai-api)
+(def version "0.1.0")
+(def class-dir "target/classes")
+(def basis (b/create-basis {:project "deps.edn"}))
+(def jar-file (format "target/%s-%s.jar" (name lib) version))
+
+(defn clean [_]
+      (b/delete {:path "target"}))
+
+(defn jar [_]
+      (b/write-pom {:class-dir class-dir
+                    :lib       lib
+                    :version   version
+                    :basis     basis
+                    :src-dirs  ["src"]})
+      (b/copy-dir {:src-dirs   ["src"]
+                   :target-dir class-dir})
+      (b/jar {:class-dir class-dir
+              :jar-file  jar-file}))
