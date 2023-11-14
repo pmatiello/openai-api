200d229ddbf7104230f6d883e585d6a3a11e2b50
Pedro Matiello <pedro@pmatiello.me>
2023-11-13 21:24:20 -0300

    Remove from model specs attributes no longer returned by OpenAI API.

--- src/me/pmatiello/openai_api/specs/model.clj
+++ src/me/pmatiello/openai_api/specs/model.clj
@@ -5,15 +5,9 @@
 (s/def ::id string?)
 (s/def ::object string?)
 (s/def ::owned-by string?)
-(s/def ::parent any?)
-(s/def ::root string?)
-
-(s/def ::permission* map?)
-(s/def ::permission
-  (s/coll-of ::permission*))
 
 (s/def ::description
-  (s/keys :req-un [::created ::id ::object ::owned-by ::parent ::permission ::root]))
+  (s/keys :req-un [::created ::id ::object ::owned-by]))
 
 (s/def ::data
   (s/coll-of ::description))
